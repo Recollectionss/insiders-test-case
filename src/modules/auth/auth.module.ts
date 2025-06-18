f41686f6e1a75bf3fb3aa4d4d2de1f71ce81b379
Observersss <heavyobjectics@gmail.com>
2025-06-18 14:53:00 +0300

    create jwt.module.config.ts and use in auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,18 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtService } from './jwt/jwt.service';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from '../../config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtModuleConfig } from './config/jwt.module.config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, JwtService],
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({ load: [jwtConfig] }),
+    JwtModule.registerAsync(jwtModuleConfig),
+  ],
 })
 export class AuthModule {}
