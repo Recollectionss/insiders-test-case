e74f90cf11675139daa5647e83253e737497f8c5
Observersss <heavyobjectics@gmail.com>
2025-06-18 10:33:46 +0300

    write Dockerfile, add app.config.ts

---
+++ src/config/app.config.ts
@@ -0,0 +1,7 @@
+import {registerAs} from "@nestjs/config";
+import * as process from "node:process";
+
+export default registerAs('app', () => ({
+    node_env: process.env.NODE_ENV,
+    port: process.env.PORT,
+}))
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import {ConfigService, ConfigType} from "@nestjs/config";
+import appConfig from "./config/app.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService)
+  const appConf:ConfigType<typeof appConfig> = configService.get('app')
+  await app.listen(appConf.port);
 }
 bootstrap();
