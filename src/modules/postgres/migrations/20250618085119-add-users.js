ad4ecfe842195ad25ded3158517c7aad4100ac09
Observersss <heavyobjectics@gmail.com>
2025-06-18 12:00:38 +0300

    add .sequelizerc, write migration for user table

---
+++ src/modules/postgres/migrations/20250618085119-add-users.js
@@ -0,0 +1,37 @@
+'use strict';
+const schema = process.env.NODE_ENV === 'production' ? 'public' : 'test';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      { tableName: 'users', schema },
+      {
+        id: {
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4,
+          primaryKey: true,
+          allowNull: false,
+          unique: true,
+        },
+        username: {
+          type: Sequelize.STRING,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        password: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+      },
+    );
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable({ tableName: 'users', schema });
+  },
+};
