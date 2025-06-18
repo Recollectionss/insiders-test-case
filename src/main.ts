1d3aaa163aec700e0ca9f85451699c67259c5c14
Observersss <heavyobjectics@gmail.com>
2025-06-18 11:59:18 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService, ConfigType } from '@nestjs/config';
 import appConfig from './config/app.config';
 import postgresConfig from './config/postgres.config';
 import { postgresTestConnection } from './modules/postgres/postgres.test.connection';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,13 @@ async function bootstrap() {
   const appConf: ConfigType<typeof appConfig> = configService.get('app');
   const postgresConf: ConfigType<typeof postgresConfig> =
     configService.get('postgres');
+
+  const config = new DocumentBuilder()
+    .setTitle('Events')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(appConf.port);
   await postgresTestConnection(postgresConf, appConf);
